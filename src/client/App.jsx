1e17dad56f4647a70ca39d2d66401ebd1e44ae29
Oliv-19 <113481931+Oliv-19@users.noreply.github.com>
2026-07-06 18:16:11 -0400

    Add basic canvas

---
+++ src/client/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Canvas } from './Canvas'
+
+function App() {
+  
+
+  return (
+    <>
+      <Canvas/>
+    </>
+  )
+}
+
+export default App
---
+++ src/client/Canvas.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import { useEffect } from "react"
+import { useRef } from "react"
+import CanvasDraw from "react-canvas-draw"
+
+export function Canvas(){
+   
+    
+    return (
+        <>
+        <div className="">
+            <CanvasDraw
+            />
+        </div> 
+        </>
+    )
+}
