36702778e598bb5376e02e1a1347c58ccc7259da
Oliv-19 <113481931+Oliv-19@users.noreply.github.com>
2026-07-07 15:24:44 -0400

    Fix canvas dimensions

--- src/client/Canvas.jsx
+++ src/client/Canvas.jsx
@@ -5,19 +5,35 @@ import * as fabric from 'fabric'
 export function Canvas(){
     const canvasRef = useRef(null)
     const {brushColor, brushSize, canvasSize} = useCanvas()
-    const [isDrawing, setIsDrawing] = useState(false)
+    const [canvas, setCanvas] = useState(null)
 
     useEffect(()=> {
-        const canvas = new fabric.Canvas(canvasRef.current, {
-            width: canvasSize.w,
-            height: canvasSize.h,
-            isDrawingMode:true
+        const dpr = window.devicePixelRatio
+        const fabricCanvas = new fabric.Canvas(canvasRef.current, {
+            width: canvasSize.w * dpr,
+            height: canvasSize.h * dpr,
+            isDrawingMode:true,
+            enableRetinaScaling: true,
         }) 
-        canvas.freeDrawingBrush = new fabric.PencilBrush(canvas)
-        canvas.freeDrawingBrush.color = brushColor
-        canvas.freeDrawingBrush.width = brushSize
+        fabricCanvas.setZoom(dpr)
+        fabricCanvas.freeDrawingBrush = new fabric.PencilBrush(fabricCanvas)
+        fabricCanvas.freeDrawingBrush.decimate = 8
+        fabricCanvas.freeDrawingBrush.color = brushColor
+        fabricCanvas.freeDrawingBrush.width = parseInt(brushSize, 10)
+        fabricCanvas.freeDrawingBrush.strokeLineCap = 'round'
+        fabricCanvas.freeDrawingBrush.strokeLineJoin = 'round'
+        setCanvas(fabricCanvas)
         
-        return () => {canvas.dispose()}
+        return () => {fabricCanvas.dispose()}
+    },[])
+    
+    useEffect(()=> {
+        if(canvas){
+            canvas.freeDrawingBrush = new fabric.PencilBrush(canvas)
+            canvas.freeDrawingBrush.color = brushColor
+            canvas.freeDrawingBrush.width = parseInt(brushSize, 10)
+        }
+
     },[brushColor, brushSize])
 
     return (
