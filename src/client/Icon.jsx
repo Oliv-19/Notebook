3be30b1263e40871e7030c38f26f1c66f1d1a65c
Oliv-19 <113481931+Oliv-19@users.noreply.github.com>
2026-08-12 11:26:17 -0400

    Style notebooks

--- src/client/Icon.jsx
+++ src/client/Icon.jsx
@@ -34,7 +34,19 @@ export function Icon({iconName, style}){
         </svg>,
         plane: <div className={`${style && style} w-2 border-l-2 border-b-2 h-4`}>
             <title>Add coordinate system</title>
-        </div>
+        </div>,
+        edit: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <title>notebook-edit</title>
+            <path d="M21.04 13.13C21.18 13.13 21.31 13.19 21.42 13.3L22.7 14.58C22.92 14.79 22.92 15.14 22.7 15.35L21.7 16.35L19.65 14.3L20.65 13.3C20.76 13.19 20.9 13.13 21.04 13.13M19.07 14.88L21.12 16.93L15.06 23H13V20.94L19.07 14.88M3 7V5H5V4C5 2.89 5.9 2 7 2H13V9L15.5 7.5L18 9V2H19C20.05 2 21 2.95 21 4V10L11 20V22H7C5.95 22 5 21.05 5 20V19H3V17H5V13H3V11H5V7H3M5 7H7V5H5V7M5 11V13H7V11H5M5 17V19H7V17H5Z" />
+        </svg>,
+        delete_notebook: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <title>notebook-remove</title>
+            <path d="M3 7V5H5V4C5 2.9 5.9 2 7 2H13V9L15.5 7.5L18 9V2H19C20 2 21 3 21 4V13.8C20.1 13.3 19.1 13 18 13C14.7 13 12 15.7 12 19C12 20.1 12.3 21.1 12.8 22H7C5.9 22 5 21 5 20V19H3V17H5V13H3V11H5V7H3M5 5V7H7V5H5M5 19H7V17H5V19M5 13H7V11H5V13M20.1 15.5L18 17.6L15.9 15.5L14.5 16.9L16.6 19L14.5 21.1L15.9 22.5L18 20.4L20.1 22.5L21.5 21.1L19.4 19L21.5 16.9L20.1 15.5Z" />
+        </svg>,
+        three_dots: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <title>dots-horizontal</title>
+            <path d="M16,12A2,2 0 0,1 18,10A2,2 0 0,1 20,12A2,2 0 0,1 18,14A2,2 0 0,1 16,12M10,12A2,2 0 0,1 12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12M4,12A2,2 0 0,1 6,10A2,2 0 0,1 8,12A2,2 0 0,1 6,14A2,2 0 0,1 4,12Z" />
+        </svg>
     }
     return (
         <>
--- src/client/Notebooks/Notebooks.jsx
+++ src/client/Notebooks/Notebooks.jsx
@@ -5,6 +5,7 @@ import { useAuth } from '../AuthContext'
 import { useCanvas } from '../Canvas/CanvasContext'
 import { useNotebooks } from './NotebooksContext'
 import { useEffect } from 'react'
+import { Icon } from '../Icon'
 
 function NewNotebookModal({close}){
     const navigate = useNavigate()
@@ -40,11 +41,13 @@ function NewNotebookModal({close}){
 function Notebook({notebook}){
     const [isEdit, setIsEdit]= useState(false)
     const [newName, setNewName]= useState(notebook.name)
+    const [openOptions, setOpenOptions]= useState(false)
     const {getNotebooks} = useNotebooks()
     const updateNotebook = ()=> {
         saveCurrNotebook(notebook) 
     }
     const delNotebook = async()=> {
+        setOpenOptions(false)
         await deleteNotebook(notebook.id)
         getNotebooks()
     }
@@ -56,17 +59,34 @@ function Notebook({notebook}){
     }
     return(
         <>
-        <div className="bg-(--green) w-40 h-50 flex flex-col items-center">
-            <div className="flex gap-2">
-                <button onClick={delNotebook} 
-                    className='cursor-pointer w-5 h-5 bg-white'>
-                        X
-                </button>
-                <button onClick={()=> {setIsEdit(prev => !prev)}} 
-                    className='cursor-pointer w-fit h-5 bg-white'>
-                        Edit
-                </button>
-            </div>
+        <div className="bg-(--dark-green) w-60 h-40 flex flex-col items-center 
+            rounded-2xl  shadow-gray-800 shadow-xl/40 relative">
+            <button className='cursor-pointer absolute top-3 right-3' onClick={()=> {setOpenOptions(true)}}>
+                <Icon iconName={'three_dots'} style={'w-7 fill-white'}/>
+            </button>
+            <div onClick={()=> {setOpenOptions(false)}} 
+                    className={`${openOptions? 'block': 'hidden'} z-2 fixed inset-0 
+                    bg-black/5 transition-opacity` }/>
+            {openOptions && 
+                <div className="flex gap-2 justify-start z-3 absolute top-10 -right-30
+                    bg-(--green) w-40 p-1 h-fit rounded flex-col">
+                    <button onClick={()=> {
+                            setIsEdit(prev => !prev)
+                            setOpenOptions(false)
+                        }} 
+                        className='cursor-pointer w-full bg-transparent flex 
+                        font-medium text-white hover:bg-(--dark-green) py-1 px-2 justify-start gap-2 items-center'>
+                            <Icon iconName={'edit'} style={'fill-white w-6'}/>
+                            Edit title
+                    </button>
+                    <button onClick={delNotebook} 
+                        className='cursor-pointer w-full bg-transparent flex 
+                        font-medium text-white hover:bg-(--dark-green) py-1 px-2 justify-start gap-2 items-center'>
+                            <Icon iconName={'delete_notebook'} style={'fill-white w-6'}/>
+                            Delete
+                    </button>
+                </div>
+            }
             {isEdit ? (
                 <>
                 <div className="h-full w-full">
@@ -82,7 +102,11 @@ function Notebook({notebook}){
                             to={`/notebooks/${notebook.name}`} 
                             className='h-full w-full'
                         >
-                        {notebook.name}
+                        <p className='w-full h-full flex justify-center text-white 
+                            font-medium first-letter:uppercase lowercase text-xl 
+                            items-center'>
+                            {notebook.name}
+                        </p>
                     </Link>
                 </>
             )
